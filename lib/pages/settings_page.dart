d312f9d86fe5ef69d8e6ac6d2a4759552a5c13b9
zesheee <ipadamov82@gmail.com>
2023-06-26 20:21:04 +1000

    fix

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -10,43 +10,54 @@ class SettingsScreen extends StatefulWidget {
 }
 
 class _SettingsScreen extends State<SettingsScreen> {
-  double _currentSliderPrimaryValue = 0.2;
-  double _currentSliderSecondaryValue = 0.5;
+  double _currentSliderPrimaryValue = 100;
+  // double _currentSliderSecondaryValue = 0.5;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Home page")),
-      body: SettingsList(
-        sections: [
-          SettingsSection(
-            tiles: <SettingsTile>[
-              SettingsTile.navigation(
-                title: const Text('Сбросить рекомендации'),
-                onPressed: (context) =>
-                    _showResetRecommendationsDialog(context),
+      appBar: AppBar(title: const Text("Настройки")),
+      body: Stack(
+        children: [
+          Container(height: double.infinity, color: Colors.black),
+          SettingsList(
+            sections: [
+              SettingsSection(
+                tiles: <SettingsTile>[
+                  SettingsTile.navigation(
+                    title: const Text('Сбросить рекомендации'),
+                    onPressed: (context) =>
+                        _showResetRecommendationsDialog(context),
+                  ),
+                  SettingsTile.navigation(
+                    title: const Text('Очистить кэш'),
+                    onPressed: (context) => _showResetCashDialog(context),
+                  ),
+                  SettingsTile(
+                    title: const Text('Установить количество хранимых данных'),
+                    value: Text('Текущее количество хранимых данных ${_currentSliderPrimaryValue}'),
+                  ),
+                ],
               ),
-              SettingsTile.navigation(
-                title: const Text('Очистить кэш'),
-                onPressed: (context) => _showResetCashDialog(context),
-              ),
-              SettingsTile(
-                title: const Text('Установить количество хранимых данных'),
-              ),
+              CustomSettingsSection(
+                  child: Slider(
+                    value: _currentSliderPrimaryValue,
+                    max: 1000,
+                    // min: 100,
+                    divisions: 20,
+                    // secondaryTrackValue: _currentSliderSecondaryValue,
+                    label: _currentSliderPrimaryValue.round().toString(),
+                    onChanged: (double value) {
+                      setState(() {
+                        _currentSliderPrimaryValue = value;
+                      });
+                    },
+                  )
+              )
             ],
           ),
-          CustomSettingsSection(
-              child: Slider(
-            value: _currentSliderPrimaryValue,
-            secondaryTrackValue: _currentSliderSecondaryValue,
-            label: _currentSliderPrimaryValue.round().toString(),
-            onChanged: (double value) {
-              setState(() {
-                _currentSliderPrimaryValue = value;
-              });
-            },
-          ))
-        ],
+        ]
+
       ),
     );
   }
@@ -57,17 +68,17 @@ void _showResetRecommendationsDialog(BuildContext context) {
     context: context,
     builder: (BuildContext context) {
       return AlertDialog(
-        title: const Text("Внимание!"),
-        content: const Text("Очистить кэш? Это действие необратимо!"),
+        title: const Text('Внимание!', style: TextStyle(color: Colors.black)),
+        content: const Text("Сбросить рекомендации? Это действие необратимо!", style: TextStyle(color: Colors.black)),
         actions: <Widget>[
           TextButton(
-            child: const Text("Отмена"),
+            child: const Text("Отмена", style: TextStyle(color: Colors.black)),
             onPressed: () {
               Navigator.of(context).pop();
             },
           ),
           TextButton(
-            child: const Text("Очистить кэш"),
+            child: const Text("Сбросить рекомендации", style: TextStyle(color: Colors.black)),
             onPressed: () {
               Navigator.of(context).pop();
             },
@@ -83,17 +94,17 @@ void _showResetCashDialog(BuildContext context) {
     context: context,
     builder: (BuildContext context) {
       return AlertDialog(
-        title: const Text("Внимание!"),
-        content: const Text("Сбросить рекомендации? Это действие необратимо!"),
+        title: const Text("Внимание!", style: TextStyle(color: Colors.black)),
+        content: const Text('Очистить кэш? Это действие необратимо!', style: TextStyle(color: Colors.black)),
         actions: <Widget>[
           TextButton(
-            child: const Text("Отмена"),
+            child: const Text("Отмена", style: TextStyle(color: Colors.black)),
             onPressed: () {
               Navigator.of(context).pop();
             },
           ),
           TextButton(
-            child: const Text("Сбросить рекомендации"),
+            child: const Text("Очистить кэш", style: TextStyle(color: Colors.black)),
             onPressed: () {
               Navigator.of(context).pop();
             },
