d096547ddac77cd2075c8a09534b7ad14bcbb349
appleolza <applepolza@mail.ru>
2023-06-26 18:46:30 +1000

    ChaCol

--- lib/main.dart
+++ lib/main.dart
@@ -23,9 +23,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'MovApp',
+
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        scaffoldBackgroundColor: Colors.blueGrey,
+        scaffoldBackgroundColor: Colors.black,
         primaryTextTheme: Typography(platform: TargetPlatform.iOS).white,
         textTheme: Typography(platform: TargetPlatform.iOS).white,
         // This is the theme of your application.
@@ -37,7 +38,7 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.indigo,
+        primarySwatch: Colors.lightGreen,
 
       ),
       // home: const MyHomePage(title: 'Flutter Demo Home Page'),
